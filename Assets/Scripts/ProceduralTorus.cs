2af9b20b9ed910ee3c18a176bfd0e10d4306565d
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-03-07 22:30:29 +0100

    Torus UV

--- Assets/Scripts/ProceduralTorus.cs
+++ Assets/Scripts/ProceduralTorus.cs
@@ -12,6 +12,8 @@ namespace ProceduralMeshes
         private int _largeCircleResolution = 16;
         [SerializeField, Min(3)]
         private int _smallCircleResolution = 16;
+        [SerializeField]
+        private Vector2Int _uvOffset;
 
         [ContextMenu("Generate")]
         public override void Generate()
@@ -20,24 +22,25 @@ namespace ProceduralMeshes
             _meshFilter.mesh = mesh;
 
             Vector3[] vertices = new Vector3[_smallCircleResolution * _largeCircleResolution];
+            Vector2[] uv = new Vector2[vertices.Length];
+            int[] triangles = new int[_smallCircleResolution * _largeCircleResolution * 6];
 
-            for (int i = 0, v = 0; i < this._largeCircleResolution; ++i)
+            for (int i = 0, v = 0; i < _largeCircleResolution; ++i)
             {
                 float largeTheta = (Mathf.PI * 2f * i) / _largeCircleResolution;
                 Vector3 smallCircleCenter = new Vector3(Mathf.Sin(largeTheta) * _largeCircleRadius, 0f, Mathf.Cos(largeTheta) * _largeCircleRadius);
 
-                for (int j = 0; j < this._smallCircleResolution; ++j, ++v)
+                for (int j = 0; j < _smallCircleResolution; ++j, ++v)
                 {
                     float smallTheta = (Mathf.PI * 2f * j) / _smallCircleResolution;
                     Vector3 pointLocalSpace = new Vector3(Mathf.Sin(smallTheta) * _smallCircleRadius, Mathf.Cos(smallTheta) * _smallCircleRadius, 0f);
                     pointLocalSpace = Quaternion.Euler(0f, largeTheta * Mathf.Rad2Deg + 90f, 0f) * pointLocalSpace;
                     
                     vertices[v] = smallCircleCenter + pointLocalSpace;
+                    uv[v] = new Vector2((i + _uvOffset.x) / (float)_largeCircleResolution % 1, (j + (_smallCircleResolution * 0.5f) + _uvOffset.y) / (float)_smallCircleResolution % 1);
                 }
             }
 
-            int[] triangles = new int[_smallCircleResolution * _largeCircleResolution * 6];
-            
             for (int i = 0, t = 0; i < _largeCircleResolution; ++i)
             {
                 int s = i * _smallCircleResolution;
@@ -55,6 +58,7 @@ namespace ProceduralMeshes
 
             mesh.vertices = vertices;
             mesh.triangles = triangles;
+            mesh.uv = uv;
             mesh.RecalculateNormals();
             mesh.RecalculateTangents();
         }
