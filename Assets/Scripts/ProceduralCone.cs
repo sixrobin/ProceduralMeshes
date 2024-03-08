a5a6911db2f56cdc37128db8f3428b718053d097
Robin Six <55784799+sixrobin@users.noreply.github.com>
2024-03-08 13:23:17 +0100

    Cone capped and uncapped variants

--- Assets/Scripts/ProceduralCone.cs
+++ Assets/Scripts/ProceduralCone.cs
@@ -10,13 +10,15 @@ namespace ProceduralMeshes
         private float _radius = 1f;
         [SerializeField, Min(3)]
         private int _resolution = 32;
+        [SerializeField]
+        private bool _cap = true;
 
-        public override void Generate()
+        private void GenerateWithCap()
         {
             Mesh mesh = GetNewMesh();
             _meshFilter.mesh = mesh;
 
-            Vector3[] vertices = new Vector3[_resolution * 2 + 2]; // +2 is for cap vertices.
+            Vector3[] vertices = new Vector3[_resolution * 2 + 2];
             Vector2[] uv = new Vector2[vertices.Length];
             int[] triangles = new int[_resolution * 6];
 
@@ -53,6 +55,47 @@ namespace ProceduralMeshes
             mesh.uv = uv;
             mesh.RecalculateNormals();
             mesh.RecalculateTangents();
+        }
+
+        private void GenerateWithoutCap()
+        {
+            Mesh mesh = GetNewMesh();
+            _meshFilter.mesh = mesh;
+
+            Vector3[] vertices = new Vector3[_resolution + 1];
+            Vector2[] uv = new Vector2[vertices.Length];
+            int[] triangles = new int[_resolution * 3];
+
+            Vector3 tipVertex = new Vector3(0f, _height);
+            vertices[^1] = tipVertex;
+            uv[^1] = Vector2.up;
+
+            for (int i = 0; i < _resolution; ++i)
+            {
+                float theta = (Mathf.PI * 2f * i) / _resolution;
+                Vector3 point = new Vector3(Mathf.Sin(theta), 0f, Mathf.Cos(theta)) * _radius;
+                    
+                vertices[i] = point;
+                uv[i] = new Vector2(i / (float)_resolution, 0f);
+
+                triangles[i * 3] = i;
+                triangles[i * 3 + 1] = (i + 1) % (vertices.Length - 1);
+                triangles[i * 3 + 2] = vertices.Length - 1;
+            }
+            
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.uv = uv;
+            mesh.RecalculateNormals();
+            mesh.RecalculateTangents();
+        }
+        
+        public override void Generate()
+        {
+            if (_cap)
+                GenerateWithCap();
+            else
+                GenerateWithoutCap();
         }
     }
 }
